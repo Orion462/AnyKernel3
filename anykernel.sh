4c77378512932576df59a5b9c131856216554760
Gaurav <79272711+Orion462@users.noreply.github.com>
2025-01-20 22:13:17 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=N0Kernel by EmanuelCN
+kernel.string=Magictime kernel by TIMISONG
 do.devicecheck=1
 do.modules=0
 do.systemless=1
